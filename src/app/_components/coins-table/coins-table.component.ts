ad93dcc640ca7c6a8e7b64fd79477273a05ae23c
Toinn <toinn@2nf.com.vn>
2018-02-02 15:32:36 +0700

    update code

--- src/app/_components/coins-table/coins-table.component.ts
+++ src/app/_components/coins-table/coins-table.component.ts
@@ -11,18 +11,20 @@ import * as io from 'socket.io-client';
 })
 export class CoinsTableComponent implements OnInit {
     private apiUrl = 'http://coincap.io/front';
+    private socketUrl = 'http://socket.coincap.io';
+    private eventListen = 'trades';
     private socket: SocketIOClient.Socket;
     data: any = {};
     constructor(private http: HttpClient) {
-        this.socket = io.connect('http://socket.coincap.io');
+        this.socket = io.connect(this.socketUrl);
+        this.socket.on(this.eventListen, (dataRealtime: any) => {
+            this.updateData(dataRealtime);
+        });
         this.getAllCoins();
         this.getData();
     }
 
     ngOnInit(): void {
-        this.socket.on('trades', (dataRealtime: any) => {
-            this.updateData(dataRealtime);
-        });
     }
 
     getData() {
@@ -35,6 +37,26 @@ export class CoinsTableComponent implements OnInit {
         });
     }
     updateData(data) {
-        console.log(data.msg);
+        var coin: string = 'Cryptocurrency-' + data.coin;
+        var coin_data: any = data.msg;
+        var _coinTable = $('#coins-list');
+        var row = _coinTable.find("tr#" + coin);
+        var price = _coinTable.find("tr#" + coin + " .price");
+        var change = _coinTable.find("tr#" + coin + " .change");
+        var supply = _coinTable.find("tr#" + coin + " .supply");
+        var volume = _coinTable.find("tr#" + coin + " .volume");
+        var capital = _coinTable.find("tr#" + coin + " .market_capital");
+        var _price = formatter.format(coin_data.price);
+
+        (volume).html(formatter.format(coin_data.volume), 0);
+        (capital).html(formatter.format(coin_data.mktcap), 0);
+        (supply).html(formatter.format(coin_data.supply));
+        (price).html(_price);
     }
+
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    });
 }
